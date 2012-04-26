feb4a92c8e300aae61d6c2def08a4619342f448b
Folashade O <f.okunubi@gmail.com>
2012-04-25 20:21:11 -0400

    TA checked off models

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,10 @@
 class Article < ActiveRecord::Base
+  belongs_to :category
+  
+  validates_presence_of :title, :content
+  
+  # Scopes
+  scope :alphabetical, order('title')
+  scope :active, where('active = ?', true)
+  
 end
